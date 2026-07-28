abae297c2c96b539230467485e0965746cd1fbd7
timkoffy <nikitakirilltimur@gmail.com>
2026-07-28 23:48:07 +0400

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+/*
+    todo: 
+    save shell state to return previous state after exiting program
+    resizing. also real time resizing why not
+    parse config file
+    ask user to set up config on first execution
+    custom art in top-left corner
+    flexible width of habit's names column
+    storing data in binary file
+*/
+
+int main(int argc, char *argv[])
+{
+    
+
+    return EXIT_SUCCESS;
+}
